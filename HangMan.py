cb9c39a326abd1af9e47e524810072fd65ad4266
Kelvinrodz13 <71534872+Kelvinrodz13@users.noreply.github.com>
2020-10-25 22:15:02 -0400

    Update,new functions
    New functions to create a txt file to store created words and read from the file.

--- HangMan.py
+++ HangMan.py
@@ -2,10 +2,11 @@ from graphics import *
 import random
 
 def Search(word='',selected_Word='',i=0):
-    if word[0] == selectedWord[i]:
+    if word[0] == selected_Word[i]:
         return True
     else:
         return False
+
 
 
 def drawHang(att=0,ledge=Point(0,0),win=GraphWin): # function to create the hanged man
@@ -44,8 +45,6 @@ def drawHang(att=0,ledge=Point(0,0),win=GraphWin): # function to create the hang
     LegLineL = Line(centerLeg,left_leg)
     LegLineR = Line(centerLeg,right_leg)
 
-
-
     if att == 1:
         head.setFill('black')
         head.draw(win)
@@ -61,119 +60,151 @@ def drawHang(att=0,ledge=Point(0,0),win=GraphWin): # function to create the hang
         LegLineR.draw(win)
 
 
+## function that create word txt file Done
+# function that reads word from file Done
+# create main Done
+# Implement button class
 
+def CreateWordFile(fileName):
+    data = open(fileName,'x')
+    wordCount = input("How many word do you wish to add?: ")
+    wordList = list()
 
-words = ['nun','sky','dog','cat','fox','gum','die','gun','fun','car','ray']
-dash = list('___')
+    for i in wordCount:
+        wordList.append(input("Enter word: "))
+    
+    data.writelines(wordList)
 
-win = GraphWin('HangMan',600,600)
-win.setCoords(0,0,600,600)
+    data.close()
 
-randomNum = random.randint(0,10)
+def ReadWordFile(fileName):
+    data = open(fileName)
+    wordList = list()
 
-selectedWord = words[randomNum]
-
-title_str = "GUESS THE WORD OR YOUR FRIEND DIES!\nYOU HAVE 6 FAIL ATTEMPTS\nCLICK ANYWHERE TO START\n"
-
-attempts = 0
-
-Title = Text(Point(300,500),title_str)
-Title.draw(win)
-####################################################Building Post
-hangPost_Base = Line(Point(250,200), Point(350,200))
-hangPost_Base.draw(win)
-postCoords = hangPost_Base.getCenter()
-postCoords.move(0,200)
-
-hangPost_Col = Line(hangPost_Base.getCenter(),postCoords)
-hangPost_Col.draw(win)
-
-ledge_x = postCoords
-
-hangPost_ledge = Line(ledge_x,Point(360,400))
-hangPost_ledge.draw(win)
-########################################################
-
-centerCol = hangPost_Col.getCenter()
-centerCol.move(-100,0)
-
-#guessWord = Text(centerCol,'none')
-#guessWord.draw(win)
-#Text()
-
-wordCount = len(selectedWord)
-
-finalCompare = "lorem"
+    for lines in data.readlines():
+        for words in lines:
+            wordList.append(words)
+    
+    return wordList
 
 
 
-win.getMouse()
+def main():
 
-while attempts < 6 and selectedWord != finalCompare:
+    #words = ['nun','sky','dog','cat','fox','gum','die','gun','fun','car','ray']
+
+    fileName = input('Enter the file name for word list >> ')
+
+    CreateWordFile(fileName)
+
+    words = ReadWordFile(fileName) 
+
+    
+
+    win = GraphWin('HangMan',600,600)
+    win.setCoords(0,0,600,600)
+
+    randomNum = random.randint(0,10)
+
+    selectedWord = words[randomNum]
+
+    dash = list('_' * len(selectedWord))
+
+    title_str = "GUESS THE WORD OR YOUR FRIEND DIES!\nYOU HAVE 6 FAIL ATTEMPTS\nCLICK ANYWHERE TO START\n"
+
+    attempts = 0
+
+    Title = Text(Point(300,500),title_str)
+    Title.draw(win)
+    ####################################################Building Post
+    hangPost_Base = Line(Point(250,200), Point(350,200))
+    hangPost_Base.draw(win)
+    postCoords = hangPost_Base.getCenter()
+    postCoords.move(0,200)
+
+    hangPost_Col = Line(hangPost_Base.getCenter(),postCoords)
+    hangPost_Col.draw(win)
+
+    ledge_x = postCoords
+
+    hangPost_ledge = Line(ledge_x,Point(360,400))
+    hangPost_ledge.draw(win)
+    ########################################################
+
+    centerCol = hangPost_Col.getCenter()
+    centerCol.move(-100,0)
+
+    #guessWord = Text(centerCol,'none')
+    #guessWord.draw(win)
+    #Text()
+
+    wordCount = len(selectedWord)
+
+    finalCompare = ""
+
+
+
+    win.getMouse()
+
+    while attempts < 6 and selectedWord != finalCompare:
+
+        attempt_str = "You have {} attempts left".format(abs(attempts-6))
+
+        Title.setText(attempt_str)
+
+        textEntry = Entry(Point(300,50),1)
+        textEntry.setFill(color_rgb(167, 197, 253))
+        if attempts < 7:
+
+            textEntry.draw(win)
+        else:
+            textEntry.undraw()
+
+        clickWord = Text(Point(300,25), 'Click anywhere to enter guess!')
+        clickWord.draw(win)
+        
+        win.getMouse() #click function as enter
+        clickWord.undraw()
+
+        user_Guess = textEntry.getText()
+
+        while user_Guess == '':
+            win.getMouse() #click function as enter
+            clickWord.undraw()
+            user_Guess = textEntry.getText()
+
+        found = str.find(selectedWord,user_Guess)
+
+        if found < 0:
+            found = False
+        else:
+            found = True
+
+        if found == True:
+
+            for i in range(wordCount):
+                if Search(user_Guess,selectedWord,i) == True:  
+                        dash[i] = user_Guess
+        else:
+            attempts += 1
+            drawHang(attempts,hangPost_ledge.getP2(),win)
+
+        finalCompare= "".join(dash)
+
+
+        userBox = Text(Point(300,150), dash)
+        userBox.draw(win)
+
 
     attempt_str = "You have {} attempts left".format(abs(attempts-6))
 
     Title.setText(attempt_str)
+    textEntry.undraw()
 
-    textEntry = Entry(Point(300,50),1)
-    textEntry.setFill(color_rgb(167, 197, 253))
-    if attempts < 7:
+    gameOver_str = 'You lost the game!, your word was: {}'.format(selectedWord)
 
-        textEntry.draw(win)
-    else:
-        textEntry.undraw()
-
-    clickWord = Text(Point(300,25), 'Click anywhere to enter guess!')
+    clickWord.setText(gameOver_str)
     clickWord.draw(win)
-    
-    win.getMouse() #click function as enter
-    clickWord.undraw()
-
-    user_Guess = textEntry.getText()
-
-    while user_Guess == '':
-        win.getMouse() #click function as enter
-        clickWord.undraw()
-        user_Guess = textEntry.getText()
-
-    found = str.find(selectedWord,user_Guess)
-
-    if found < 0:
-        found = False
-    else:
-        found = True
-
-    if found == True:
-
-        for i in range(wordCount):
-            if Search(user_Guess,selectedWord,i) == True:  
-                    dash[i] = user_Guess
-    else:
-        attempts += 1
-        drawHang(attempts,hangPost_ledge.getP2(),win)
-    
-    #guessWord.undraw()
-
-   #guessWord = Text(centerCol, dash)
-    #guessWord.setText(dash)
-    #guessWord.draw(win)
-
-    finalCompare= "".join(dash)
-
-
-    userBox = Text(Point(300,150), dash)
-    userBox.draw(win)
-
-
-attempt_str = "You have {} attempts left".format(abs(attempts-6))
-
-Title.setText(attempt_str)
-textEntry.undraw()
-
-gameOver_str = 'You lost the game!, your word was: {}'.format(selectedWord)
-
-clickWord.setText(gameOver_str)
-clickWord.draw(win)
-win.getMouse()
-win.close()
+    win.getMouse()
+    win.close()
 
